4c06a65b06d1366a7c604a7f264989a690894eb7
Muhammad Umair <umair.leo17@gmail.com>
2025-11-20 18:46:20 +0000

    feat(config): github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    host: true
-  }
-})
+    host: true,
+  },
+  base: "/nutrient-predictor-frontend/",
+});
